a3c79d7a46eb4df39aa2bfe8065ae0549ae31f43
bhagyaj <bhagyajkumar1@gmail.com>
2022-12-06 22:11:27 +0530

    initial commit

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react";
+import { ButtonGroup, Card, Button, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+
+export default function Auth(){
+
+
+    return(
+        <Container>
+            <Card>
+                <Card.Header>
+                    <h1>Authentication</h1>
+                </Card.Header>
+                <Card.Body>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
